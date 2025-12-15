f9d1fdd881cc72c96d5f204c46a1ba27fe54c0a6
Fabricio-Guima <fsgkof@gmail.com>
2025-12-15 19:26:00 -0300

    Config pattern datetime iso 8601

--- course/src/main/java/com/ead/course/models/ModuleModel.java
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -31,7 +31,7 @@ public class ModuleModel implements Serializable {
     private String description;
 
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
 
     //    quero sempre associar um modulo a um curso por isso o false
